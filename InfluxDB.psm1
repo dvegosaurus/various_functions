3543e7a2502876051252b58f7d72a0b5dd5aba9b
dveg <49659252+dvegosaurus@users.noreply.github.com>
2021-02-19 19:40:02 +0100

    Update InfluxDB.psm1
    added a class to write to InfluxDB (beta)

--- InfluxDB.psm1
+++ InfluxDB.psm1
@@ -15,3 +15,41 @@ function get-timestamp
 
     $timestamp
 } # get-timestamp -timeoffset -1
+
+class influxDBwriter
+{
+    [string]$uri
+    [string]$port
+    [string]$measurement
+    [INT64]$timestamp
+
+    influxDBwriter(){
+        $this.timestamp = [INT64]((New-TimeSpan -Start ([timezone]::CurrentTimeZone.ToLocalTime((get-date "01/01/1970 00:00:00"))) -End (Get-Date)).TotalSeconds) *1000000000
+    }
+
+    influxDBwriter($uri,$port){
+        $this.uri = $uri
+        $this.port = $port
+        $this.timestamp = [INT64]((New-TimeSpan -Start ([timezone]::CurrentTimeZone.ToLocalTime((get-date "01/01/1970 00:00:00"))) -End (Get-Date)).TotalSeconds) *1000000000
+    }
+
+    [void]writedata($string,[bool]$sendTimestamp){
+        $params = @{
+            uri = "$($this.uri):($this.port)/write?db=$($this.measurement)"
+            body = $string
+            method = "post"
+        }
+        invoke-restmethod @params
+    }
+
+    [void]updateTimestamp(){
+        $this.timestamp = [INT64]((New-TimeSpan -Start ([timezone]::CurrentTimeZone.ToLocalTime((get-date "01/01/1970 00:00:00"))) -End (Get-Date)).TotalSeconds) *1000000000
+    }
+
+}
+
+
+#$influxDB = [influxDBwriter]::new()
+#$influxDB.port = "8086"
+#$InfluxDB.uri = "http://localhost"
+#$influxDB.updateTimestamp()
