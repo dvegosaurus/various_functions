9fc9ea013276760bb2362acd2204906040f35ce3
dveg <49659252+dvegosaurus@users.noreply.github.com>
2022-03-27 15:27:39 +0200

    Update keepass.ps1
    added concatenation of several keepass in one database

--- keepass.ps1
+++ keepass.ps1
@@ -2,6 +2,8 @@ $path = 'C:\Program Files (x86)\KeePass Password Safe 2'
 [Reflection.Assembly]::LoadFile("$path\KeePass.exe") | Out-Null
 [Reflection.Assembly]::LoadFile("$path\KeePass.XmlSerializers.dll") | Out-Null
 
+# prerequisites install-module PoShKeePass
+
 function get-keepass {
     param(
         $path,
@@ -39,11 +41,11 @@ function get-KeepassEntries {
 
 $files = @(
     @{
-        path = "T:\perso.kdbx"
-        pass = 'xxxxxxxx'
+        path = "T:\xxxxxxxx.kdbx"
+        pass = 'xxxxxxxxx'
     }
     @{
-        path =  "O:\client.kdbx"
+        path =  "O:\xxxxxxxx.kdbx"
         pass = 'xxxxxxxxxxx'
     }
 )
@@ -52,4 +54,43 @@ $data = foreach ($file in $files){
     get-keepass -path $file.path  -password $file.Pass  | get-KeepassEntries
 }
 
-$data | Out-GridView
+
+# create credential for the new Keepass
+$user = "toto"
+$pwd = ConvertTo-SecureString "toto" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user,$pwd)
+
+# create the keepass
+$dbname = "kp"
+$dbprofilename = "test"
+$dbpath = "C:\Temp\$dbname.kdbx"
+
+New-KeePassDatabase -DatabasePath C:\temp\kp.kdbx -MasterKey $cred
+Get-KeePassDatabaseConfiguration | Remove-KeePassDatabaseConfiguration -Confirm:$false
+$kpdatabase = New-KeePassDatabaseConfiguration -DatabasePath $dbpath -DatabaseProfileName $dbprofilename -UseMasterKey 
+
+# import the keepass entries into the new keepass
+foreach ($item in $data){
+    # if the group does not exist create it
+    $groups = get-KeePassGroup -MasterKey $cred -DatabaseProfileName $dbprofilename
+    $groupPath = "$dbname/$($item.Keepass)"
+    if ($groupPath -notin $groups.fullpath){
+        New-KeePassGroup -MasterKey $cred -DatabaseProfileName $dbprofilename -KeePassGroupName $item.Keepass -KeePassGroupParentPath $dbname
+    }
+
+    # splatting for cleaner code
+    $params = @{
+        Title                 = $item.Title
+        KeePassPassword       = $item.Password | ConvertTo-SecureString -AsPlainText -Force
+        DatabaseProfileName   = $dbprofilename
+        KeePassEntryGroupPath = $groupPath
+        MasterKey             = $cred        
+    }
+    if ($item.URL){$params += @{URL= $item.URL}} # URL might be empty so we add it only if it has content
+    if ($item.Notes){$params += @{Notes= $item.Notes}}
+    if ($item.UserName.Length -ne 0){$params += @{UserName = $item.UserName}} else {$params += @{UserName = ' '}}
+
+    New-KeePassEntry  @params 
+
+
+}
